612f7be5228a4f17c36b1854e826c101cc4fef6b
paul19pv <paul_19pv@hotmail.com>
2018-05-20 18:39:23 -0500

    registro pancho

--- application/models/Persona_model.php
+++ application/models/Persona_model.php
@@ -7,7 +7,7 @@ class Persona_model extends CI_Model {
     }
     public function get_persona()
     {
-        $query = $this->db->get_where('persona', array('idpersona' => '1'));
+        $query = $this->db->get_where('persona', array('idpersona' => '2'));
         return $query->row_array();
     }
 }
